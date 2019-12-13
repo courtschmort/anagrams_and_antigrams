6b0dddf89c5cde47e15b5ddea005acbff5cdcd9b
Courtney Schild <courtneyschild@gmail.com>
2019-12-13 10:16:10 -0800

    add a method called 'checker' to check if two words are anagrams; pass test.

--- lib/anaanti.rb
+++ lib/anaanti.rb
@@ -1,6 +1,17 @@
 class AnaAnti
-  def initialize
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
+  end
 
+  def checker()
+    word1 = @word1
+    word2 = @word2
+    arr1 = word1.chars.sort
+    arr2 = word2.chars.sort
+    if arr1.eql?(arr2)
+      "These words are anagrams."
+    end
   end
 
 end
