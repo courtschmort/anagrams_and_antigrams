8a9c1a15c050f2d20ab0a8d0f78b5563d54da347
Courtney Schild <courtneyschild@gmail.com>
2019-12-13 11:16:57 -0800

    refactor to add method called 'word_checker' to separate (future) logic for phrases, words and vowels.

--- lib/anaanti.rb
+++ lib/anaanti.rb
@@ -1,20 +1,23 @@
 class AnaAnti
-  def initialize(word1, word2)
-    @word1 = word1
-    @word2 = word2
+  def initialize(input1, input2)
+    @input1 = input1
+    @input2 = input2
     @vowels = ["a", "e", "i", "o", "u", "y"]
   end
 
   def ana_anti_checker
-    word1 = @word1.downcase
-    word2 = @word2.downcase
+  end
+
+  def word_checker
+    word1 = @input1.downcase
+    word2 = @input2.downcase
     arr1 = word1.chars.sort
     arr2 = word2.chars.sort
     if arr1.eql?(arr2) then return "These words are anagrams." end
   end
 
   def vowel_checker
-    if !@vowels.include?(@word1) | !@vowels.include?(@word2) then return "You need to input actual words!" end
+    if !@vowels.include?(@input1) | !@vowels.include?(@input2) then return "You need to input actual words!" end
   end
 
 end
--- spec/anaanti_spec.rb
+++ spec/anaanti_spec.rb
@@ -4,11 +4,11 @@ require('anaanti')
 describe('#AnaAnti') do
   it('should check if two words are anagrams') do
     anaanti = AnaAnti.new("ruby", "bury")
-    expect(anaanti.ana_anti_checker()).to(eq("These words are anagrams."))
+    expect(anaanti.word_checker()).to(eq("These words are anagrams."))
   end
   it('should account for the possibility that words might have different cases but should still be anagrams') do
     anaanti = AnaAnti.new("Ruby", "bury")
-    expect(anaanti.ana_anti_checker()).to(eq("These words are anagrams."))
+    expect(anaanti.word_checker()).to(eq("These words are anagrams."))
   end
   it('should check if the inputs are words') do
     anaanti = AnaAnti.new("mrs", "mrs")
